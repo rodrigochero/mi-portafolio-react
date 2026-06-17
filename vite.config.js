f06fe066f82b5a8c6410a3b74c224369d29c87da
Rodrigo Salvador <rs9183786@gmail.com>
2026-06-16 21:54:55 -0500

    fix: configurar base path para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mi-portafolio-react/', // 👈 Esto le dice a Vite que el proyecto se ejecutará en tu subcarpeta de GitHub
 })
