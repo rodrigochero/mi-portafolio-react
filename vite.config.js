c4ff623b5f3895ed0eab720ec8e3d7f1c122d929
Rodrigo Salvador <rs9183786@gmail.com>
2026-06-16 23:38:34 -0500

    fix: cambiar base path a raiz para despliegue en vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-portafolio-react/',
+  base: '/', // 👈 Ajustado a '/' para que Vercel encuentre tus rutas e imágenes al instante
 })
